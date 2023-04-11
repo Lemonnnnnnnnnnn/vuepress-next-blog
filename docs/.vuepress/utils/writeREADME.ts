6b9dba8ecc5f5edb2de8b4bd3d97dbc5534814d9
linyuchen <1041707577@qq.com>
2023-04-11 14:33:38 +0800

    添加ts-node依赖调试ts代码

--- docs/.vuepress/utils/writeREADME.ts
+++ docs/.vuepress/utils/writeREADME.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-
 // 获取unix系统的分隔符
 const sep = path.sep;
 // 获取windows系统的分隔符
@@ -16,20 +15,30 @@ const source_dir_pure = source_dir.slice(0, source_dir.length - 1); // remove la
 function fn(dir: string): void {
     // let results: string[] = [];
     const list = fs.readdirSync(dir);
+    
+    // 判断是否有子文件夹
+    let hasSubDir = false;
+    const _list = list.filter((file) => file.indexOf('.vuepress') === -1)
 
-    // 判断是否有子目录
-    const hasSubDir = list.some((file) => {
+    for (let file of _list) {
         file = dir + systemSep + file;
-    })
+        
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            hasSubDir = true
+        }
+    }
 
     if (hasSubDir) {
+        console.log(_list);
+        
         const readmePath = dir + systemSep + 'README.md';
         // 获取source_dir后面的路径
         const file_path = dir.slice(source_dir_pure.length);
         let readmeContent = ''
 
         // 遍历子文件夹
-        for (let file of list) {
+        for (let file of _list) {
             readmeContent += `## ${file} \
             [${file}](${file_path}/${file}) \n`;
             fn(dir + systemSep + file)
@@ -42,25 +51,6 @@ function fn(dir: string): void {
         const readmeContent = `# ${dir.split(systemSep).pop()}`;
         fs.writeFileSync(readmePath, readmeContent);
     }
-
-    // for (let file of list) {
-    //     file = dir + systemSep + file;
-    //     const stat = fs.statSync(file);
-    //     if (stat && stat.isDirectory()) {
-    //         if (file.indexOf('.vuepress') !== -1) continue;
-    //         // results = results.concat(getFiles(file));
-    //     } else {
-    //         // results.push(file);
-
-    //         // 将当前文件夹目录名写入README.md
-    //         const readmePath = file.replace(/\.md$/, '') + systemSep + 'README.md';
-    //         const readmeContent = `# ${dir.split(systemSep).pop()}`;
-    //         fs.writeFileSync(readmePath, readmeContent);
-    //     }
-
-    // }
-
-    // return results;
 }
 
 fn(source_dir_pure);
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,58 @@
-# docs
+---
+layout: 'Home'
+projects:
+  - name: Vue
+    link: /vue/
+    icon: /vue-js-icon.svg
+
+  - name: React
+    link: /react/
+    icon: /react-js-icon.svg
+
+  - name: Rust
+    link: /rust/
+    icon: /rustacean-orig-noshadow.svg
+
+  - name: Java
+    link: /java/
+    icon: /java-programming-language-icon.svg
+
+  - name: JavaScript
+    link: /javaScript/
+    icon: /javascript-programming-language-icon.svg
+
+  - name: Css
+    link: /css/
+    icon: /css-file-format-icon.svg
+
+  - name: Python
+    link: /python/
+    icon: /python-programming-language-icon.svg
+
+  - name: Sql
+    link: /sql/
+    icon: /sql-icon.svg
+
+  - name: Server
+    link: /server/
+    icon: /ubuntu-color-icon.svg
+
+  - name: Tools
+    link: /tools/
+    icon: /git-icon.svg
+
+  - name: Webpack
+    link: /webpack/
+    icon: /webpack-icon.svg
+
+  - name: Cs
+    link: /cs/
+    icon: /tech-icon.svg
+
+  - name: Life
+    link: /life/
+    icon: /life.svg
+
+---
+
+<!-- # Hello VuePress -->
