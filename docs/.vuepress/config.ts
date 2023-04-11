639c078443bddbd2706927ff014d35a6195f98eb
soul <104170577@qq.com>
2023-04-11 21:17:11 +0800

    feat: change base path

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,6 +5,7 @@ import { getSideBar } from './utils/getSideBar'
 
 export default defineUserConfig({
   lang: 'zh-CN',
+  base : '/vuepress-next-blog/',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
   theme: defaultTheme({
